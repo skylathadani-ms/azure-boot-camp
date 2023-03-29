025403e514bb13efe5e5074f450c54f6b9f879d4
Skyla Thadani <skylathadani@microsoft.com>
2023-03-29 16:37:53 -0700

    done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,35 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import sentiment from 'sentiment';
 import './App.css';
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState('');
   const [articles, setArticles] = useState([]);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const response = await axios.get(
-      `https://newsapi.org/v2/everything?q=${searchTerm}&apiKey=7e725d089e1344659ed123634947279d&pageSize=10`
-    );
-    setArticles(response.data.articles);
-  };
+  useEffect(() => {
+    const fetchArticles = async () => {
+      const response = await axios.get(
+        `https://newsapi.org/v2/everything?q=microsoft%20finance&apiKey=7e725d089e1344659ed123634947279d&pageSize=10`
+      );
+      const articlesWithSentiment = response.data.articles.map((article) => {
+        const sentimentResult = sentiment(article.description || '');
+        const score = sentimentResult && sentimentResult.score;
+        const sentimentScore = score ? Math.round((score + 5) / 10 * 100) : null;
+        const color = sentimentScore && sentimentScore > 50 ? 'green' : 'inherit';
+        return { ...article, sentimentScore, color };
+      });
+      setArticles(articlesWithSentiment);
+    };
+
+    fetchArticles();
+  }, []);
 
   return (
     <div className="container">
-      <h1>Microsoft News Search</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          placeholder="Enter search term"
-        />
-        <button type="submit">Search</button>
-      </form>
+      <h1>Microsoft Finance News</h1>
       {articles.map((article) => (
-        <div key={article.url} className="article">
+        <div key={article.url} className="article" style={{ color: article.color }}>
           <h2>{article.title}</h2>
           <p>{article.description}</p>
+          {article.sentimentScore && (
+            <p>Sentiment Score: {article.sentimentScore}%</p>
+          )}
           <a href={article.url}>Read More</a>
         </div>
       ))}
@@ -38,3 +42,7 @@ function App() {
 }
 
 export default App;
+
+
+
+
